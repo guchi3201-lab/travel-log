b51f7ffa1f93bb95c468894c450030178974b2eb
guchi3201-lab <guchi3201@gmail.com>
2026-01-26 16:51:08 +0900

    Change map center to Japan and add Tokyo marker
    Updated map center to Japan's geographical center and added a marker for Tokyo.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const map = L.map("map").setView([35.681236, 139.767125], 5);
+// 日本を中心に地図表示
+const map = L.map("map").setView([36.2048, 138.2529], 5);
 
+// 地図タイル
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors"
 }).addTo(map);
+
+// テスト用ピン（東京）
+L.marker([35.681236, 139.767125])
+  .addTo(map)
+  .bindPopup("東京");
