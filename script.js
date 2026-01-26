eb3fb47c9ecea80d534ca556f42692dfb41d6fc9
guchi3201-lab <guchi3201@gmail.com>
2026-01-26 16:54:48 +0900

    Add markers for multiple locations on the map

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 L.marker([35.681236, 139.767125])
   .addTo(map)
   .bindPopup("東京");
+const spots = [
+  { name: "太宰府天満宮", lat: 33.5215, lng: 130.5346 },
+  { name: "清水寺", lat: 34.9948, lng: 135.7850 },
+  { name: "浅草寺", lat: 35.7148, lng: 139.7967 },
+  { name: "厳島神社", lat: 34.2950, lng: 132.3199 },
+  { name: "札幌時計台", lat: 43.0621, lng: 141.3544 }
+];
+
+spots.forEach(s => {
+  L.marker([s.lat, s.lng])
+    .addTo(map)
+    .bindPopup(s.name);
+});
